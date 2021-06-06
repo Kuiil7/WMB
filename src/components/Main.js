e1a2ef4d44de90a351ee8fed01b0fb8208bc8dfd
kuiil7 <kuiil7ig11@gmail.com>
2021-06-05 17:49:35 -0700

    added google-maps-react helpers and subcomponents  Marker and position info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import {  GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, GoogleApiWrapper, Marker  } from 'google-maps-react';
+import { DateTime } from 'luxon';
+
 require('dotenv').config()
 
 
 const mapStyles = {
-  width: '100%',
+  width: '99%',
   height: '100%'
 }
 
@@ -93,10 +95,95 @@ const SubmitEvent = <div className="field is-grouped">
 
   return (
 
-     <div className="column is-three-fifths is-offset-one-fifth">
+     <div className="column ">
 
+<div className="column is-three-fifths is-offset-one-fifth">
 
 {SubmitEvent}
+</div>
+
+
+<Map
+
+google={this.props.google}
+
+initialCenter={{
+  lat: 38.4528,
+  lng: -99.9065
+}}
+zoom={4}
+//onReady={this.onReady}
+
+style={mapStyles}
+
+bounds={this.state.bounds}
+
+>
+
+{
+
+
+            this.state.breweries.map( (brewery, j) =>
+
+              <Marker
+              key={j}
+              name={brewery.name}
+              street={brewery.street}
+              city={brewery.city}
+              state={brewery.state}
+              brewery_type={brewery.brewery_type}
+              country={brewery.country}
+              website_url={brewery.website_url}
+              phone={brewery.phone}
+              updated_at={brewery.updated_at}
+              position={{ lat: brewery.latitude, lng: brewery.longitude}}
+              onClick={this.infoWindowHendler}
+              />
+            )
+          }
+
+
+
+  <InfoWindow
+             marker={this.state.activeMarker}
+             visible={this.state.showInfoWindow}
+             onClose={this.onCloseWindow}
+         >
+         <div className="box  container ">
+<p className="title ">{this.state.selectedPlace.name}
+<br />
+
+<button className="button is-focus is-outline  is-success">{this.state.selectedPlace.brewery_type}</button>
+
+</p>
+
+<p className="subtitle p-2 ">{this.state.selectedPlace.street}, {this.state.selectedPlace.city}, {this.state.selectedPlace.state},  {this.state.selectedPlace.country}
+
+<br />
+
+<a href="tel:{this.state.selectedPlace.phone}" className=" is-hovered ">
+{this.state.selectedPlace.phone}
+</a>
+
+
+
+<br />
+<a href= {this.state.selectedPlace.website_url} className=" is-primary is-hovered">
+{this.state.selectedPlace.website_url}
+</a>
+</p>
+<p>
+ <a href="tel:{this.state.selectedPlace.phone}" className=" is-hovered">
+{this.state.selectedPlace.phone}
+</a>
+</p>
+<p className="has-text-right h as-text-grey-lighter is-italic">
+Last updated:  {DateTime.fromISO(this.state.selectedPlace.updated_at).toFormat('LLL dd yyyy')}
+</p>
+
+ </div>
+         </InfoWindow>
+</Map>
 
      </div>
 
